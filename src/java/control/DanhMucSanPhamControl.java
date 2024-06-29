76207a52b0bf8bc8ce24c202ba49496114bb0ee1
trunghieu2803 <dev.hieunguyen03@gmail.com>
2024-06-29 11:31:36 +0700

    update product load 1 page

--- src/java/control/DanhMucSanPhamControl.java
+++ src/java/control/DanhMucSanPhamControl.java
@@ -12,6 +12,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 import java.util.List;
 
 /**
@@ -35,16 +36,51 @@ public class DanhMucSanPhamControl extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         DaoSanPham daoSP = new DaoSanPham();
         DaoDanhMucSanPham daoDMSP = new DaoDanhMucSanPham();
-        
-        String cid = request.getParameter("cid");
-        
+
+        String cid = request.getParameter("test");
+
         List<SanPham> ListSP = daoSP.getAllSanPhamByMSP(cid);
         request.setAttribute("ListSP", ListSP);
-       
+
         List<DanhMucSanPham> ListDMSP = daoDMSP.getAllDanhMucSanPham();
         request.setAttribute("ListDMSP", ListDMSP);
-        request.setAttribute("tag", cid);
-        request.getRequestDispatcher("Home.jsp").forward(request, response);
+
+        PrintWriter out = response.getWriter();
+        for (SanPham o : ListSP) {
+            out.println("<div class=\"col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item women\">\n"
+                    + "                            <!-- Block2 -->\n"
+                    + "\n"
+                    + "                            <div class=\"block2\">\n"
+                    + "                                <div class=\"block2-pic hov-img0\">\n"
+                    + "                                    <img src=\"images/"+o.getAnh()+"\" alt=\"IMG-PRODUCT\">\n"
+                    + "\n"
+                    + "                                    <button class=\"block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1\">\n"
+                    + "                                        Quick View\n"
+                    + "                                    </button>\n"
+                    + "                                </div>\n"
+                    + "\n"
+                    + "                                <div class=\"block2-txt flex-w flex-t p-t-14\">\n"
+                    + "                                    <div class=\"block2-txt-child1 flex-col-l \">\n"
+                    + "                                        <a href=\"product-detail.html\" class=\"stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6\">\n"
+                    + "                                            "+o.getTensanpham()+"\n"
+                    + "                                        </a>\n"
+                    + "\n"
+                    + "                                        <span class=\"stext-105 cl3\">\n"
+                    + "                                            "+o.getGiatien()+"\n"
+                    + "                                        </span>\n"
+                    + "                                    </div>\n"
+                    + "\n"
+                    + "                                    <div class=\"block2-txt-child2 flex-r p-t-3\">\n"
+                    + "                                        <a href=\"#\" class=\"btn-addwish-b2 dis-block pos-relative js-addwish-b2\">\n"
+                    + "                                            <img class=\"icon-heart1 dis-block trans-04\" src=\"images/icons/icon-heart-01.png\" alt=\"ICON\">\n"
+                    + "                                            <img class=\"icon-heart2 dis-block trans-04 ab-t-l\" src=\"images/icons/icon-heart-02.png\" alt=\"ICON\">\n"
+                    + "                                        </a>\n"
+                    + "                                    </div>\n"
+                    + "                                </div>\n"
+                    + "                            </div>\n"
+                    + "                        </div>");
+        }
+
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
