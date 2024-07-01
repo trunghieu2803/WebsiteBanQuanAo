8c375c37741db647e012c3e913e93a950e9ee2fd
Devcujx2112 <Vutungduongnight@gmail.com>
2024-07-02 01:31:31 +0700

    CRUD admin

---
+++ sql/QuanLyDeCao_full.sql
@@ -0,0 +1,8 @@
+SELECT TOP (1000) [donhang_id]
+      ,[khachhang_id]
+      ,[ngaydathang]
+      ,[tongtien]
+      ,[trangthai]
+  FROM [QuanLyDeCao].[dbo].[DonHang]
+  select * from DonHang
+  insert into DonHang values(3,'2003/12/21',23,'test')
